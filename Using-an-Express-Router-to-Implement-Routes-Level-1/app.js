009f6ceba9b66f4e14def6b341656dd48e477814
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2023-09-14 00:02:54 +0530

    Using-an-Express-Router-to-ImplementRoutes-Level-1

--- Using-an-Express-Router-to-Implement-Routes-Level-1/app.js
+++ Using-an-Express-Router-to-Implement-Routes-Level-1/app.js
@@ -1,78 +1,68 @@
 const fs = require("fs");
 const express = require("express");
-const router = express.Router();
+const app = express();
+const router = new express.Router();
+
+//middleware
+router.use(express.json());
 
 //Including product.json file
 const product = JSON.parse(fs.readFileSync(`${__dirname}/product.json`));
-
 // Defining The Router
 // Get all the products
-router.get("/", (req, res) => {
+router.get("/api/v1/product", (req, res) => {
 	try {
-		//Write your code here
-		res.status(200).send({
+		res.status(200).json({
 			status: "success",
 			results: product.length,
 			data: {
-				product,
+				product: product,
 			},
 		});
 	} catch (error) {
 		res.status(400).json(error);
 	}
 });
-
-async function saveDataToDatabase(data) {
-	return new Promise((resolve, reject) => {
-		const jsonData = JSON.stringify(data);
-
-		fs.writeFile("./product.json", jsonData, (err) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve();
-			}
-		});
-	});
-}
-
-//Create a new Product
-router.post("/", (req, res) => {
+//Creating new Product
+router.post("/api/v1/product", (req, res) => {
 	try {
-		//Write your code here
-		let newId = product[product.length - 1].id + 1;
-
-		let { title, price } = req.body;
-		// console.log(newId, lastProduct);
-
-		const newProduct = {
-			id: newId,
-			title,
-			price,
-		};
-
+		const { title, price } = req.body;
 		if (!title || !price) {
-			res.status(404).send({
+			// If either title or price is missing, send a single error response
+			return res.status(404).json({
 				message: "Title and price are required",
 				status: "Error",
 			});
-		} else if (price && title) {
-			product.push(newProduct);
-			saveDataToDatabase(product);
-
-			res.status(200).send({
-				status: "success",
+		}
+		const newId = product[product.length - 1].id + 1;
+		const newProduct = { id: newId, title, price };
+		product.push(newProduct);
+		fs.writeFile(`${__dirname}/product.json`, JSON.stringify(product), (err) => {
+			// If there is an error writing the file, send an error response
+			if (err) {
+				return res.status(500).json({
+					message: "Error creating product",
+					status: "Error",
+				});
+			}
+			// Otherwise, send the success response
+			res.status(201).json({
+				status: "Success",
 				data: {
 					product: newProduct,
 				},
 			});
-		}
+		});
 	} catch (error) {
-		res.status(400).send({
+		// If there is an unhandled error, send an error response
+		res.status(400).json({
 			message: "Error creating product",
 			status: "Error",
 		});
 	}
 });
 
-module.exports = router;
+//Registering our Router
+app.use(router);
+
+module.exports = app;
--- Using-an-Express-Router-to-Implement-Routes-Level-1/index.js
+++ Using-an-Express-Router-to-Implement-Routes-Level-1/index.js
@@ -1,17 +1,5 @@
-const express = require("express");
-const app = express();
-
+const app = require("./app");
 const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
-const productRoute = require("./app");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//Router Middlewares
-app.use(express.json());
-app.use(bodyParser.json());
-
-app.use("/api/v1/product", productRoute);
 
 dotenv.config();
 
